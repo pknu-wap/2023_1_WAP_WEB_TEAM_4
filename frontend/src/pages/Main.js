947e401d7f6fca314c3d39fdcd95418663a633b2
chaeyeon1 <slide10245@naver.com>
2023-03-23 22:44:30 +0900

    div 태그 추가

--- frontend/src/pages/Main.js
+++ frontend/src/pages/Main.js
@@ -4,15 +4,25 @@ function Main() {
   return (
     <div
       style={{
-        backgroundColor: "black",
-        width: "300px",
-        height: "300px",
-        color: "white",
+        backgroundColor: "aqua",
         display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
+        flexDirection: "row",
       }}>
-      Main
+      <div
+        style={{
+          backgroundColor: "black",
+          // background-color: "white"
+          width: "300px",
+          height: "300px",
+          color: "white",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          borderRadius: "20%",
+        }}>
+        Main
+      </div>
+      <div style={{ width: "300px", height: "300px" }}></div>
     </div>
   );
 }
