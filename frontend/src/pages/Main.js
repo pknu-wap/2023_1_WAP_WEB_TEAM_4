1d6fb037596ba93a05db25ac12c4a4768fdfb94a
chaeyeon1 <slide10245@naver.com>
2023-03-23 19:27:26 +0900

    css 속성 정리

--- frontend/src/pages/Main.js
+++ frontend/src/pages/Main.js
@@ -1,7 +1,20 @@
 import React from "react";
 
 function Main() {
-  return <div>Main</div>;
+  return (
+    <div
+      style={{
+        backgroundColor: "black",
+        width: "300px",
+        height: "300px",
+        color: "white",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}>
+      Main
+    </div>
+  );
 }
 
 export default Main;
