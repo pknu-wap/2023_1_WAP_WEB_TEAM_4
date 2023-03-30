a9a649368a90a0d6c66fe3261c1b88420888afa0
sungwookim <rooney423@naver.com>
2023-03-30 22:21:42 +0900

    div, style, 가로:justifyContent, 세로: alighitems

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 import Main from "./pages/Main";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
-
+import Mypage from "./pages/Mypage"; 
 const App = () => {
   return (
     <Router>
@@ -11,6 +11,7 @@ const App = () => {
         <Route path="/" element={<Main />} />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/mypage" element={<Mypage />} />
       </Routes>
     </Router>
   );
--- frontend/src/pages/Mypage.js
+++ frontend/src/pages/Mypage.js
@@ -1,7 +1,59 @@
 import React from "react";
 
 function Mypage() {
-  return <div>Mypage</div>;
+  return (
+    <div style={{ width: "100%", height: "3000px", backgroundColor: "black" }}>
+      <div
+        style={{
+          width: "100%",
+          height: "50px",
+          backgroundColor: "white",
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "space-between",
+        }}
+      >
+        <div
+          style={{ width: "10%", height: "100%", backgroundColor: "yellow" }}
+        ></div>
+        <div
+          style={{
+            width: "20%",
+            height: "100%",
+            backgroundColor: "blue",
+            display: "flex",
+            flexDirection: "row",
+          }}
+        >
+          <div
+            style={{ width: "50%", height: "100%", backgroundColor: "green" }}
+          ></div>
+
+          <div
+            style={{ width: "50%", height: "100%", backgroundColor: "red" }}
+          ></div>
+        </div>
+      </div>
+      <div
+        style={{
+          width: "100%",
+          height: "500px",
+          backgroundColor: "#ECD8A4",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <div
+          style={{ width: "50%", height: "100px", backgroundColor: "orange" }}
+        >
+          <div>비밀번호 확인</div>
+          <input type="text" style={{ width: "100%", height: "30px" }}></input>
+          <button>확인</button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Mypage;
