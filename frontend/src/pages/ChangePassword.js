8d138713395ce1c0348d527867c6a2b413450734
sungwookim <rooney423@naver.com>
2023-04-04 21:13:26 +0900

    changepassword test
    sadf

--- frontend/src/pages/ChangePassword.js
+++ frontend/src/pages/ChangePassword.js
@@ -1,7 +1,18 @@
 import React from "react";
-
+import Header from "../components/Header";
 function ChangePassword() {
-  return <div>asdf</div>;
+  return (
+    <div>
+      <Header />
+      <div
+        style={{
+          marginTop: "100px",
+          width: "100px",
+          height: "50px",
+          backgroundColor: "red",
+        }}
+      ></div>
+    </div>
+  );
 }
-
 export default ChangePassword;
