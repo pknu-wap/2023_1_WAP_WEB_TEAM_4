0f13eed1c501fdbf8b2edf83f388e3b51f520245
chaeyeon1 <slide10245@naver.com>
2023-04-04 20:46:07 +0900

    changePassword 페이지 router 만들기

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,9 @@ import "./App.css";
 import Main from "./pages/Main";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
-import Mypage from "./pages/Mypage"; 
+import Mypage from "./pages/Mypage";
+import ChangePassword from "./pages/ChangePassword";
+
 const App = () => {
   return (
     <Router>
@@ -12,6 +14,7 @@ const App = () => {
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
         <Route path="/mypage" element={<Mypage />} />
+        <Route path="/changePassword" element={<ChangePassword />} />
       </Routes>
     </Router>
   );
