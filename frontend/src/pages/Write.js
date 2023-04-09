24022e3c86f960c3f69e164139103b94d9294e0d
chaeyeon1 <slide10245@naver.com>
2023-04-09 22:42:40 +0900

    코드 정리

--- frontend/src/pages/Write.js
+++ frontend/src/pages/Write.js
@@ -1,17 +1,18 @@
 import React, { useState } from "react";
-import { Stack, Chip, Button, IconButton, TextField } from "@mui/material";
-import SettingsIcon from "@mui/icons-material/Settings";
+import { Stack, Chip, Button, Snackbar, Alert } from "@mui/material";
 import { useRef } from "react";
 import { Editor } from "@toast-ui/react-editor";
-import { Viewer } from "@toast-ui/react-editor";
 import "@toast-ui/editor/dist/toastui-editor.css";
 import "@toast-ui/editor/dist/theme/toastui-editor-dark.css";
-
+import CancelIcon from "@mui/icons-material/Cancel";
 import "@toast-ui/editor/dist/i18n/ko-kr";
 
 const Write = () => {
   const editorRef = useRef();
   const [text, setText] = useState("");
+  const [tagArray, setTagArray] = useState([]);
+  const [tag, setTag] = useState("");
+  const [snackbarOpen, setSnackbarOpen] = useState(false);
 
   // 등록 버튼 핸들러
   const handleRegisterButton = () => {
@@ -50,6 +51,7 @@ const Write = () => {
           spacing={2}
           padding="40px 96px 40px 96px"
           bgcolor="black"
+          direction="column"
           width="100%">
           <input
             placeholder="제목을 입력해주세요"
@@ -59,41 +61,98 @@ const Write = () => {
               outline: "none",
               backgroundColor: "transparent",
               fontSize: "24px",
+              fontWeight: "bold",
             }}
           />
-
-          <Stack bgcolor="#d9d9d9" width="10%" height="1px"></Stack>
-
           <Stack
             display="flex"
             alignItems="center"
             height="60px"
             justifyContent="space-between"
             direction="row">
-            {/* <Stack
+            <Stack
               bgcolor="black"
               paddingTop="10px"
               spacing={1}
               color="white"
               direction="row">
-              <Chip label="# 파이썬" />
-              <Chip label="# 파이썬" />
-              <Chip label="# 파이썬" />
-              <Chip label="# 파이썬" />
-            </Stack> */}
-            <input
-              placeholder="태그를 입력해주세요"
-              onKeyDown={(event) => {
-                if (event.keyCode === 32) {
-                  console.log("안녕");
-                }
-              }}
-              style={{
-                color: "white",
-                border: "none",
-                outline: "none",
-                backgroundColor: "transparent",
-              }}></input>
+              {tagArray.map((tag, i) => {
+                return (
+                  <Chip
+                    sx={{
+                      border: "1px solid #ECD8A4",
+                      "& .MuiChip-deleteIcon": {
+                        color: "#F3F4F6",
+                        ":hover": {
+                          color: "#D1D5DB",
+                        },
+                        ":active": {
+                          color: "#9CA3AF",
+                        },
+                      },
+                    }}
+                    key={i}
+                    label={`# ${tag}`}
+                    deleteIcon={<CancelIcon fontSize="12px" />}
+                    onDelete={() =>
+                      setTagArray(tagArray.filter((tag, index) => index !== i))
+                    }
+                  />
+                );
+              })}
+              <input
+                placeholder="태그를 입력해주세요"
+                value={tag}
+                onChange={(event) => {
+                  setTag(event.target.value);
+                }}
+                onKeyDown={(event) => {
+                  if (event.keyCode === 32) {
+                    if (tagArray.length <= 10) {
+                      if (!tag.includes("#")) {
+                        setTagArray([...tagArray, tag]);
+                        setTag("");
+                      } else {
+                        setTag("");
+                        setSnackbarOpen(true);
+                        return (
+                          <Snackbar
+                            open={snackbarOpen}
+                            autoHideDuration={6000}
+                            onClose={() => snackbarOpen(false)}>
+                            <Alert
+                              onClose={() => snackbarOpen(false)}
+                              severity="success"
+                              sx={{ width: "100%" }}>
+                              태그에 '#'은 포함될 수 없습니다.
+                            </Alert>
+                          </Snackbar>
+                        );
+                      }
+                    } else {
+                      setTag("");
+                      setSnackbarOpen(true);
+                      return (
+                        <Snackbar open={snackbarOpen} autoHideDuration={6000}>
+                          <Alert
+                            onClose={() => snackbarOpen(false)}
+                            severity="success"
+                            sx={{ width: "100%" }}>
+                            태그의 최대 개수는 10개입니다.
+                          </Alert>
+                        </Snackbar>
+                      );
+                    }
+                  }
+                }}
+                style={{
+                  color: "white",
+                  border: "none",
+                  outline: "none",
+                  backgroundColor: "transparent",
+                }}
+              />
+            </Stack>
             <Stack direction="row">
               <Button
                 disableRipple
