0e257441c55e529d84f33cff9f14d7f8eaba6547
a54005107 <a01054005107@gmail.com>
2023-04-11 20:46:00 +0900

    게시글 레포지토리

--- backend/src/main/java/com/project/glog/repository/MemoryContentRepository.java
+++ backend/src/main/java/com/project/glog/repository/MemoryContentRepository.java
@@ -18,11 +18,16 @@ public class MemoryContentRepository implements ContentRepository{
 
     @Override
     public Content update(Content content) {
-        return null;
+        store.put(content.getId(), content);
+        return content;
     }
 
     @Override
     public void delete(Long id) {
+        Content content = entityManager.find(Content.class, Id);
+        if(content != null){
+            entityManager.remove(content);
+        }
 
     }
 
@@ -33,14 +38,18 @@ public class MemoryContentRepository implements ContentRepository{
 
     @Override
     public List<Content> findContentsByHashtag(String hashtag) {
-        return null;
-    }
+        String query = "SELECT p FROM Post p WHERE :hashTag MEMBER OF p.hashTags";
+        TypedQuery<Content> typedQuery = entityManager.createQuery(query, Content.class);
+        typedQuery.setParameter("hashTag", hashTag);
+        return typedQuery.getResultList();
+    } //
 
     @Override
     public List<Content> previewsByCreated() {
-
-        return null;
-    }
+        String jpql = "SELECT p FROM Post p ORDER BY p.createdDate DESC";
+        TypedQuery<Content> query = entityManager.createQuery(jpql, Post.class);
+        return query.getResultList();
+    }//생성일자 기준으로 게시글 목록을 조회하여 날짜 순서대로 정렬된 리스트 반환
 
     @Override
     public List<Content> previewsByLikes() {
@@ -65,7 +74,22 @@ public class MemoryContentRepository implements ContentRepository{
 
     @Override
     public List<Content> previewsByViews() {
-        return null;
+        ArrayList<Map.Entry<Long, Content>> map_list = new ArrayList<>(store.entrySet());
+
+        Collections.sort(map_list, new Comparator<Map.Entry<Long, Content>>() {
+            @Override
+            public int compare(Map.Entry<Long, Content> o1, Map.Entry<Long, Content> o2) {
+                // 값(Value)을 기준으로 비교
+                return o1.getValue().getViews().compareTo(o2.getValue().getViews());
+            }
+        });
+
+        List<Content> list = new ArrayList<>();
+        for (Map.Entry<Long, Content> value : map_list) {
+            list.add(value.getValue());
+        }
+
+        return list;
     }
 
     @Override
