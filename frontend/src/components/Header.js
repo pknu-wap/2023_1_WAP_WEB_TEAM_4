41113bce382e5e1870e0468dbe82049721caf4fc
chaeyeon1 <slide10245@naver.com>
2023-04-16 15:23:18 +0900

    모아보기 페이지 search toggle 구현

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,38 +1,63 @@
-import { Stack, Button } from "@mui/material";
+import { Stack, Button, IconButton } from "@mui/material";
 import React from "react";
+import SearchIcon from "@mui/icons-material/Search";
 import { useNavigate } from "react-router-dom";
+import HomeIcon from "@mui/icons-material/Home";
 
-const Header = () => {
+const Header = ({ isSearchOpen, setIsSearchOpen }) => {
   const navigate = useNavigate();
 
   return (
-    <Stack position="fixed" padding="10px 0px 10px 20px" bgcolor="black">
-      <Stack
-        sx={{
-          cursor: "pointer",
-        }}
-        onClick={() => navigate("/home")}
-        width="100%"
-        fontWeight="bold"
-        color="#ECD8A4"
-        fontSize="40px">
-        GLOG
+    <Stack
+      position="fixed"
+      padding="10px 0px"
+      width="100%"
+      direction="row"
+      alignItems="center"
+      justifyContent="space-between"
+      bgcolor="black"
+      zIndex={100}>
+      <Stack alignItems="flex-start" marginLeft="20px">
+        <Stack
+          sx={{
+            cursor: "pointer",
+          }}
+          onClick={() => navigate("/home")}
+          fontWeight="bold"
+          color="#ECD8A4"
+          fontSize="40px">
+          GLOG
+        </Stack>
+        <Button
+          size="small"
+          disableRipple
+          onClick={() => navigate("/")}
+          sx={{
+            height: "30px",
+            marginTop: "-5px",
+            marginLeft: "-3px",
+            color: "#EED083",
+            ":hover": {
+              color: "#F1C040",
+            },
+          }}>
+          CHAEYEON'S BLOG
+        </Button>
       </Stack>
-      <Button
-        size="small"
-        disableRipple
-        onClick={() => navigate("/")}
-        sx={{
-          height: "30px",
-          marginTop: "-5px",
-          marginLeft: "-3px",
-          color: "#EED083",
-          ":hover": {
-            color: "#F1C040",
-          },
-        }}>
-        CHAEYEON'S BLOG
-      </Button>
+      <Stack marginRight="28px" direction="row" spacing={4} alignItems="center">
+        <IconButton onClick={() => setIsSearchOpen(!isSearchOpen)}>
+          {isSearchOpen ? (
+            <HomeIcon sx={{ color: "white" }} />
+          ) : (
+            <SearchIcon sx={{ color: "white" }} />
+          )}
+        </IconButton>
+        <Stack
+          width="40px"
+          height="40px"
+          borderRadius="20px"
+          bgcolor="white"></Stack>
+      </Stack>
     </Stack>
   );
 };
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "../components/Header";
 import { IconButton, MenuItem, Select, Stack, TextField } from "@mui/material";
 import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
@@ -6,93 +6,91 @@ import ChevronRightIcon from "@mui/icons-material/ChevronRight";
 import SearchIcon from "@mui/icons-material/Search";
 
 const Home = () => {
-  const [age, setAge] = React.useState("");
+  const [selectValue, setSelectValue] = useState(0);
+  const [isSearchOpen, setIsSearchOpen] = useState(false);
+
   const handleChange = (event) => {
-    setAge(event.target.value);
+    setSelectValue(event.target.value);
   };
 
   const array = [
     {
-      class: "recent",
-      card: [
+      random: [
         {
-          title: "제목",
-          content: "내용",
+          title: "랜덤 제목",
+          main_text: "내용",
         },
         {
           title: "제목2",
-          content: "내용2",
+          main_text: "내용2",
         },
         {
           title: "제목3",
-          content: "내용3",
+          main_text: "내용3",
         },
         {
           title: "제목4",
-          content: "내용4",
+          main_text: "내용4",
         },
       ],
     },
     {
-      class: "like",
-      card: [
+      views: [
         {
-          title: "제목",
-          content: "내용",
+          title: "조회수 제목",
+          main_text: "내용",
         },
         {
           title: "제목2",
-          content: "내용2",
+          main_text: "내용2",
         },
         {
           title: "제목3",
-          content: "내용3",
+          main_text: "내용3",
         },
         {
           title: "제목4",
-          content: "내용4",
+          main_text: "내용4",
         },
       ],
     },
     {
-      class: "comment",
-      card: [
+      likes: [
         {
-          title: "제목",
-          content: "내용",
+          title: "좋아요 제목",
+          main_text: "내용",
         },
         {
           title: "제목2",
-          content: "내용2",
+          main_text: "내용2",
         },
         {
           title: "제목3",
-          content: "내용3",
+          main_text: "내용3",
         },
         {
           title: "제목4",
-          content: "내용4",
+          main_text: "내용4",
         },
       ],
     },
     {
-      class: "click",
-      card: [
+      latest: [
         {
-          title: "제목",
-          content: "내용",
+          title: "최근 제목",
+          main_text: "내용",
         },
         {
           title: "제목2",
-          content: "내용2",
+          main_text: "내용2",
         },
         {
           title: "제목3",
-          content: "내용3",
+          main_text: "내용3",
         },
         {
           title: "제목4",
-          content: "내용4",
+          main_text: "내용4",
         },
       ],
     },
@@ -100,88 +98,98 @@ const Home = () => {
 
   return (
     <Stack height="100%">
-      <Header>Home</Header>
+      <Header isSearchOpen={isSearchOpen} setIsSearchOpen={setIsSearchOpen}>
+        Home
+      </Header>
       <Stack
         spacing={10}
         height="5000px"
-        minWidth="100%"
-        width="min-content"
         bgcolor="black"
-        padding="125px 96px 40px 96px">
-        <Stack direction="row" spacing={4} justifyContent="center">
-          <Select
-            value={age}
-            onChange={handleChange}
-            sx={{
-              color: "white",
-              width: "10%",
-              "&.MuiOutlinedInput-root": {
-                "&.Mui-focused": {
-                  "& .MuiOutlinedInput-notchedOutline": {
-                    border: "1px solid #ECD8A4",
+        padding="150px 96px 40px 96px">
+        {isSearchOpen && (
+          <Stack direction="row" spacing={4} justifyContent="center">
+            <Select
+              value={selectValue}
+              onChange={handleChange}
+              sx={{
+                color: selectValue === 0 ? "gray" : "white",
+                width: "10%",
+                "&.MuiOutlinedInput-root": {
+                  "&.Mui-focused": {
+                    "& .MuiOutlinedInput-notchedOutline": {
+                      border: "1px solid #ECD8A4",
+                    },
+                  },
+                  ":hover": {
+                    "&.MuiOutlinedInput-root": {
+                      "& .MuiOutlinedInput-notchedOutline": {
+                        border: "1px solid #ECD8A4",
+                      },
+                    },
                   },
                 },
-                ":hover": {
-                  "&.MuiOutlinedInput-root": {
+              }}>
+              <MenuItem value={0} sx={{ display: "none" }}>
+                선택
+              </MenuItem>
+              <MenuItem value={10}>태그</MenuItem>
+              <MenuItem value={20}>작성자</MenuItem>
+              <MenuItem value={30}>내용</MenuItem>
+            </Select>
+            <TextField
+              placeholder="검색어를 입력해주세요"
+              InputProps={{
+                startAdornment: <SearchIcon sx={{ marginRight: "10px" }} />,
+              }}
+              sx={{
+                width: "70%",
+                "& .MuiOutlinedInput-root": {
+                  "&.Mui-focused": {
                     "& .MuiOutlinedInput-notchedOutline": {
                       border: "1px solid #ECD8A4",
                     },
                   },
                 },
-              },
-            }}>
-            <MenuItem value={10}>태그</MenuItem>
-            <MenuItem value={20}>작성자</MenuItem>
-            <MenuItem value={30}>내용</MenuItem>
-          </Select>
-          <TextField
-            InputProps={{
-              startAdornment: <SearchIcon sx={{ marginRight: "10px" }} />,
-            }}
-            sx={{
-              width: "70%",
-              "& .MuiOutlinedInput-root": {
-                "&.Mui-focused": {
-                  "& .MuiOutlinedInput-notchedOutline": {
-                    border: "1px solid #ECD8A4",
+                ":hover": {
+                  "& .MuiOutlinedInput-root": {
+                    "& .MuiOutlinedInput-notchedOutline": {
+                      border: "0.1px solid #ECD8A4",
+                    },
                   },
                 },
-              },
-              ":hover": {
-                "& .MuiOutlinedInput-root": {
-                  "& .MuiOutlinedInput-notchedOutline": {
-                    border: "0.1px solid #ECD8A4",
-                  },
-                },
-              },
-            }}
-          />
-        </Stack>
-        {array.map((sudol, index) => {
-          return (
-            <Stack>
-              <Stack color="white">{sudol.class}</Stack>
-              <Stack spacing={4} direction="row">
-                <IconButton>
-                  <ChevronLeftIcon />
-                </IconButton>
-                {sudol.card.map((cardContent, i) => {
-                  return (
-                    <Stack>
-                      <Stack width="300px" height="200px" bgcolor="white" />
-                      <Stack color="white">{cardContent.title}</Stack>
-                      <Stack color="white">{cardContent.content}</Stack>
-                    </Stack>
-                  );
-                })}
+              }}
+            />
+          </Stack>
+        )}
+        {!isSearchOpen &&
+          array.map((sudol, index) => {
+            const category = ["random", "views", "likes", "latest"];
+            return (
+              <Stack key={index}>
+                <Stack spacing={4} direction="row" justifyContent="center">
+                  <Stack color="white" fontSize="20px">
+                    {category[index]}
+                  </Stack>
+                  <IconButton sx={{ borderRadius: "0px" }}>
+                    <ChevronLeftIcon />
+                  </IconButton>
+                  {sudol[category[index]].map((cardContent, i) => {
+                    return (
+                      <Stack key={i}>
+                        <Stack width="300px" height="200px" bgcolor="white" />
+                        <Stack color="white">{cardContent.title}</Stack>
+                        <Stack color="white">{cardContent.main_text}</Stack>
+                      </Stack>
+                    );
+                  })}
 
-                <IconButton>
-                  <ChevronRightIcon />
-                </IconButton>
+                  <IconButton sx={{ borderRadius: "0px" }}>
+                    <ChevronRightIcon />
+                  </IconButton>
+                </Stack>
               </Stack>
-            </Stack>
-          );
-        })}
+            );
+          })}
       </Stack>
     </Stack>
   );
