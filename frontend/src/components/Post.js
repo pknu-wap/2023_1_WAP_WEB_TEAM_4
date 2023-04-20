94e3827aef98da6d7c19b44322d2c3ac75933043
Chaeyeon1 <66813821+Chaeyeon1@users.noreply.github.com>
2023-04-20 13:44:22 +0900

    태그들로 뿌리기

--- frontend/src/components/Post.js
+++ frontend/src/components/Post.js
@@ -1,77 +1,40 @@
-import { Button, Stack } from "@mui/material";
+import { Stack } from "@mui/material";
 import React from "react";
-import { mint } from "../static/style/color";
+import ReactHtmlParser from "react-html-parser";
 
 const Post = () => {
+  const content =
+    "<p>safasdfasdfasdf</p><h3>asdfasdf</h3><h2>sdfasdfadsf</h2><h2>#!@#!@#!@#</h2>";
   return (
     <Stack
       marginRight="100px"
       alignItems="flex-start"
       width="850px"
       marginTop="80px"
-      justifyContent="center">
+      justifyContent="center"
+    >
       <Stack>
-        <Stack direction="row" justifyContent="space-between">
-          <Stack color="black" fontSize="32px" height="45px" fontWeight="bold">
-            알고리즘에 대해 배워보자!
-          </Stack>
-          <Stack direction="row">
-            <Button sx={{ color: mint[500] }}>수정</Button>
-            <Button color="error">삭제</Button>
-          </Stack>
+        <Stack color="black" fontSize="32px" height="45px" fontWeight="bold">
+          알고리즘에 대해 배워보자!
         </Stack>
         <Stack width="850px" height="2px" bgcolor="teal" marginBottom="24px" />
         <Stack>
           <Stack
             style={{
+              margin: "0px",
               color: "black",
-              fontSize: "24px",
-              paddingBottom: "12px",
-              fontWeight: "bold",
-            }}>
-            알고리즘
-          </Stack>
-          <Stack
-            style={{
-              color: "black",
-              paddingBottom: "12px",
-              fontSize: "16px",
-            }}>
-            알고리즘은 신기하다.
-          </Stack>
-          <Stack
-            style={{
-              color: "black",
-              paddingBottom: "12px",
-              fontSize: "20px",
-              fontWeight: "bold",
-            }}>
-            알고리즘이 신기한 이유?
-          </Stack>
-          <Stack
-            style={{
-              color: "black",
-              paddingBottom: "12px",
-              fontSize: "16px",
-            }}>
-            모르겠다.
-          </Stack>
-          <Stack
-            style={{
-              color: "black",
-              paddingBottom: "12px",
-              fontSize: "20px",
-              fontWeight: "bold",
-            }}>
-            알고리즘이 안신기해지려면?
-          </Stack>
-          <Stack
-            style={{
-              color: "black",
-              paddingBottom: "12px",
-              fontSize: "15px",
-            }}>
-            모르겠다.
+            }}
+            gap="5px"
+          >
+            {ReactHtmlParser(content, {
+              transform: (node, index) => {
+                console.log(node.type);
+                console.log(node);
+                if (node.type === "tag") {
+                  node.attribs = { ...node.attribs, style: "margin: 0;" };
+                }
+              },
+            })}
           </Stack>
         </Stack>
       </Stack>
