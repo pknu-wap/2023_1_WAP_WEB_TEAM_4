bc8e2831df4eb0abccb5243085cdc5de57867d60
chaeyeon1 <slide10245@naver.com>
2023-04-22 14:41:08 +0900

    anchor 만들기

--- frontend/src/components/Anchor.js
+++ frontend/src/components/Anchor.js
@@ -1,7 +1,40 @@
 import { Stack } from "@mui/material";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { postState } from "../states/writeState";
+import { useRecoilState } from "recoil";
 
 const Anchor = () => {
+  const [sections, setSections] = useState([]);
+  const [post] = useRecoilState(postState);
+
+  useEffect(() => {
+    const regex =
+      /<h1.*?>(.*?)<\/h1>|<h2.*?>(.*?)<\/h2>|<h3.*?>(.*?)<\/h3>|<h4.*?>(.*?)<\/h4>/gs;
+    const matches = [...post.matchAll(regex)];
+    const newSections = matches.map((match, index) => ({
+      id: `section-${index + 1}`,
+      html: match[0] || "",
+      content: match[4] || match[3] || match[2] || match[1] || "",
+      isActive: false,
+      ref: React.createRef(),
+    }));
+    setSections(newSections);
+  }, []);
+
+  const handleClick = (sectionId) => {
+    setSections(
+      sections.map((section) =>
+        section.id === sectionId
+          ? { ...section, isActive: true }
+          : { ...section, isActive: false }
+      )
+    );
+    const selectedSection = sections.find(
+      (section) => section.id === sectionId
+    );
+    selectedSection?.ref?.current?.scrollIntoView({ behavior: "smooth" });
+  };
+
   return (
     <Stack
       paddingTop="30px"
@@ -12,32 +45,48 @@ const Anchor = () => {
         top: 0,
         right: 100,
       }}>
-      <Stack>
-        <Stack
-          fontWeight="bold"
-          fontSize="18px"
-          color="background.color"
-          height="35px"
-          sx={{ cursor: "pointer" }}>
-          알고리즘
-        </Stack>
-        <Stack
-          fontSize="16px"
-          color="primary.500"
-          paddingLeft="10px"
-          height="30px"
-          sx={{ cursor: "pointer" }}>
-          - 알고리즘이 신기한 이유?
-        </Stack>
-        <Stack
-          fontSize="16px"
-          color="background.color"
-          paddingLeft="10px"
-          height="30px"
-          sx={{ cursor: "pointer" }}>
-          - 알고리즘이 안신기해지려면?
-        </Stack>
-      </Stack>
+      {sections.map((section, i) => {
+        if (section?.html.startsWith("<h1")) {
+          return (
+            <Stack
+              fontWeight="bold"
+              ref={section.ref}
+              id={section.id}
+              fontSize="18px"
+              color="background.color"
+              height="35px"
+              sx={{ cursor: "pointer" }}>
+              {section.content}
+            </Stack>
+          );
+        } else if (section?.html.startsWith("<h2")) {
+          return (
+            <Stack
+              fontSize="16px"
+              color="primary.500"
+              ref={section.ref}
+              id={section.id}
+              paddingLeft="10px"
+              height="30px"
+              sx={{ cursor: "pointer" }}>
+              {section.content}
+            </Stack>
+          );
+        } else if (section?.html.startsWith("<h3")) {
+          return (
+            <Stack
+              fontSize="16px"
+              color="background.color"
+              ref={section.ref}
+              id={section.id}
+              paddingLeft="10px"
+              height="30px"
+              sx={{ cursor: "pointer" }}>
+              {section.content}
+            </Stack>
+          );
+        } else return null;
+      })}
     </Stack>
   );
 };
