cb6b7debc21898b76663cf49369da79a83c1353a
chaeyeon1 <slide10245@naver.com>
2023-04-25 23:28:05 +0900

    값 바꾸기/

--- frontend/src/states/writeState.js
+++ frontend/src/states/writeState.js
@@ -1,23 +1,12 @@
 import { atom } from "recoil";
 
-// export const postState = atom({
-//   key: "post",
-//   default: "",
-// });
-// export const titleState = atom({
-//   key: "title",
-//   default: "",
-// });
 export const postState = atom({
   key: "post",
-  // default:
-  //   "<h1>Post</h1><h2>Post</h2><h3>Post</h3><h1>Post</h1><h2>Post</h2><h3>Post</h3><h1>Post</h1><h2>Post</h2><h3>Post</h3><h1>Post</h1><h1>Post</h1><h1>Post</h1><h1>Post</h1><h1>Post</h1><h1>Post</h1><h1>Post</h1><h1>Post</h1><h1>Post</h1><h1>Post</h1><h1>Post</h1><h1>Post</h1><h1>Post</h1><h1>Post</h1><h1>Post</h1><h1>Post</h1>",
-  default:
-    "#### asdfsafasdf \n\n### asdfasdfasdf \n### asdfasdfasdf \n### asdfasdfasdf \n# asdfasdfasdf \n## asdfasdfasdf \n### asdfasdfasdf \n### asdfasdfasdf \n### asdfasdfasdf \n### asdfasdfasdf \n### asdfasdfasdf \n### asdfasdfasdf \n### asdfasdfasdf \n### asdfasdfasdf \n### asdfasdfasdf \n### asdfasdfasdf \n### asdfasdfasdf \n### asdfasdfasdf \n### asdfasdfasdf \n### asdfasdfasdf \n### asdfasdfasdf \n### asdfasdfasdf \n### asdfasdfasdf \n### asdfasdfasdf \n### asdfasdfasdf \n### asdfasdfasdf \n### asdfasdfasdf \n### asdfasdfasdf \n### asdfasdfasdf \n### asdfasdfasdf \n### asdfasdfasdf \n### asdfasdfasdf \n### asdfasdfasdf \n### asdfasdfasdf \n### asdfasdfasdf",
+  default: "",
 });
 export const titleState = atom({
   key: "title",
-  default: "Title",
+  default: "",
 });
 
 export const sectionsState = atom({
