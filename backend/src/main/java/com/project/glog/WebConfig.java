c30bc4649729eae17136911100cfb9090827616d
du yeong <doyeong32@gmail.com>
2023-05-23 20:22:47 +0900

    origin 수정

--- backend/src/main/java/com/project/glog/WebConfig.java
+++ backend/src/main/java/com/project/glog/WebConfig.java
@@ -12,7 +12,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(final CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000/")
+                .allowedOrigins("http://localhost:3000")
                 .allowedHeaders("*")
                 .allowCredentials(true)
                 .allowedMethods(ALLOWED_METHOD_NAMES.split(","))
