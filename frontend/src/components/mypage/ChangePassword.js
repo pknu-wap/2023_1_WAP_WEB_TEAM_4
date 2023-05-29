6ed82477dae8cb7baeec81b2f29b77410e198f25
chaeyeon1 <slide10245@naver.com>
2023-05-29 21:45:31 +0900

    마이페이지 레이아웃 완성

--- frontend/src/components/mypage/ChangePassword.js
+++ frontend/src/components/mypage/ChangePassword.js
@@ -5,28 +5,63 @@ import React from "react";
 function ChangePassword() {
   const theme = useTheme();
   return (
-    <Stack
-      // border={`1px solid ${theme.palette.primary.main}`}
-      width="100%"
-      bgcolor="background.contractColor"
-      alignItems="flex-start"
-      borderRadius="0px 10px 10px 0px"
-      gap="24px">
+    <>
       <Stack
-        // width="300px"
-        width="100%"
-        alignItems="center"
-        p="4%"
-        color="primary.main"
-        // borderRadius="10px"
-        // border={`1px solid ${theme.palette.primary.main}`}
-      >
-        비밀번호 확인
+        bgcolor="background.contractColor"
+        borderRadius="0px 10px 10px 0px"
+        direction="row"
+        marginTop="24px">
+        <Stack
+          width="120px"
+          alignItems="flex-start"
+          justifyContent="center"
+          fontSize="16px"
+          fontWeight={600}>
+          아이디
+        </Stack>
+        {/* <TextField size="small" fullWidth /> */}
+        <Stack
+          fontWeight="bold"
+          justifyContent="center"
+          alignItems="flex-start">
+          Chaeyeon
+        </Stack>
       </Stack>
-      <TextField size="small" fullWidth>
-        비밀번호 확인
-      </TextField>
-    </Stack>
+      <Stack
+        bgcolor="background.contractColor"
+        borderRadius="0px 10px 10px 0px"
+        direction="row"
+        gap="24px">
+        <Stack
+          width="120px"
+          alignItems="flex-start"
+          fontSize="16px"
+          justifyContent="center"
+          fontWeight={600}>
+          비밀번호
+        </Stack>
+        <TextField size="small" fullWidth>
+          비밀번호 확인
+        </TextField>
+      </Stack>
+      <Stack
+        bgcolor="background.contractColor"
+        borderRadius="0px 10px 10px 0px"
+        direction="row"
+        gap="24px">
+        <Stack
+          width="120px"
+          alignItems="flex-start"
+          fontSize="16px"
+          justifyContent="center"
+          fontWeight={600}>
+          닉네임
+        </Stack>
+        <TextField size="small" fullWidth>
+          비밀번호 확인
+        </TextField>
+      </Stack>
+    </>
   );
 }
 
--- frontend/src/pages/Mypage.js
+++ frontend/src/pages/Mypage.js
@@ -7,6 +7,7 @@ import ChangePassword from "../components/mypage/ChangePassword";
 import Post from "../components/mypage/Post";
 import Skin from "../components/mypage/Skin";
 import Blog from "../components/mypage/Blog";
+import Pororo from "../static/pic/Pororo.jpg";
 
 function Mypage() {
   const theme = useTheme();
@@ -29,110 +30,85 @@ function Mypage() {
       <Header />
 
       <Stack
-        direction="row"
-        height="100%"
-        width="50%"
         marginTop="80px"
+        width="fit-content"
+        bgcolor="background.contractColor"
+        border={`1px solid ${theme.palette.primary.main}`}
+        alignItems="flex-start"
         justifyContent="center"
-        // flexWrap="wrap"
-        // bgcolor="background.contractColor"
-        p="4%"
-        // gap="44px"
-      >
+        borderRadius="12px"
+        p="80px 64px"
+        gap="52px">
         <Stack
           borderRadius="10px"
-          border={`1px solid ${theme.palette.primary.main}`}
-          p="4%"
           width="100%"
-          maxWidth="200px"
           gap="24px"
           alignItems="center"
-          // borderRight="0px solid black"
           bgcolor="background.contractColor"
-          // gap="24px"
+          direction="row"
           height="100%">
-          <Typography variant="h6" color="primary.main" fontWeight={700}>
-            CHAEYEON's BLOG
-          </Typography>
-          <Stack
-            width="80%"
-            height="160px"
-            bgcolor="primary.main"
-            borderRadius="50%"
+          <img
+            style={{ width: "20%", height: "20%", borderRadius: "50%" }}
+            src={Pororo}
+            alt="User_Picture"
           />
-          <Stack fontSize="24px">Chae yeon</Stack>
-          <Stack>한줄 소개를 작성하세요</Stack>
-        </Stack>
-        <Stack
-          // border={`1px solid ${theme.palette.primary.main}`}
-          //   justifyContent="center"
-          width="100%"
-          //   maxWidth="900px"
-          bgcolor="background.contractColor"
-          border={`1px solid ${theme.palette.primary.main}`}
-          alignItems="flex-start"
-          justifyContent="center"
-          borderRadius="0px 10px 10px 0px"
-          borderLeft="0px solid black"
-          gap="24px"
-          direction="row">
-          <Stack width="100%" p="32px" gap="32px">
-            <Stack direction="row" width="100%">
-              <Stack
-                width="fit-content"
-                p="2% 5%"
-                borderRadius="10px"
-                alignContent="center"
-                justifyContent="center"
-                color="white"
-                bgcolor={theme.palette.primary[400]}
-                sx={{ cursor: "pointer" }}
-                onClick={() => setOption(0)}>
-                계정 설정
-              </Stack>
-              <Stack
-                width="fit-content"
-                padding="2% 5%"
-                borderRadius="10px"
-                alignContent="center"
-                justifyContent="center"
-                onClick={() => setOption(3)}
-                sx={{ cursor: "pointer" }}
-                // color="white"
-                // bgcolor={theme.palette.primary[400]}
-              >
-                블로그 설정
-              </Stack>
-              <Stack
-                width="fit-content"
-                padding="2% 5%"
-                borderRadius="10px"
-                alignContent="center"
-                justifyContent="center"
-                sx={{ cursor: "pointer" }}
-                onClick={() => setOption(1)}
-                // color="white"
-                // bgcolor={theme.palette.primary[400]}
-              >
-                방명록 설정
-              </Stack>
-              <Stack
-                width="fit-content"
-                padding="2% 5%"
-                borderRadius="10px"
-                alignContent="center"
-                sx={{ cursor: "pointer" }}
-                justifyContent="center"
-                onClick={() => setOption(2)}
-                // color="white"
-                // bgcolor={theme.palette.primary[400]}
-              >
-                스킨
-              </Stack>
-            </Stack>
-            {optionArray[option]}
+
+          <Stack width="100%" gap="32px">
+            <Stack fontSize="24px">Chae yeon</Stack>
+            <Stack>한줄 소개를 작성하세요</Stack>
           </Stack>
         </Stack>
+        <Stack direction="row" width="100%">
+          <Stack
+            width="fit-content"
+            p="2% 5%"
+            borderRadius="10px"
+            alignContent="center"
+            justifyContent="center"
+            color={option === 0 ? "white" : "black"}
+            bgcolor={option === 0 ? theme.palette.primary[400] : undefined}
+            sx={{ cursor: "pointer" }}
+            onClick={() => setOption(0)}>
+            계정 설정
+          </Stack>
+          <Stack
+            width="fit-content"
+            padding="2% 5%"
+            borderRadius="10px"
+            alignContent="center"
+            justifyContent="center"
+            onClick={() => setOption(3)}
+            sx={{ cursor: "pointer" }}
+            color={option === 3 ? "white" : "black"}
+            bgcolor={option === 3 ? theme.palette.primary[400] : undefined}>
+            블로그 설정
+          </Stack>
+          <Stack
+            width="fit-content"
+            padding="2% 5%"
+            borderRadius="10px"
+            alignContent="center"
+            justifyContent="center"
+            sx={{ cursor: "pointer" }}
+            onClick={() => setOption(1)}
+            color={option === 1 ? "white" : "black"}
+            bgcolor={option === 1 ? theme.palette.primary[400] : undefined}>
+            방명록 설정
+          </Stack>
+          <Stack
+            width="fit-content"
+            padding="2% 5%"
+            borderRadius="10px"
+            alignContent="center"
+            sx={{ cursor: "pointer" }}
+            justifyContent="center"
+            onClick={() => setOption(2)}
+            color={option === 2 ? "white" : "black"}
+            bgcolor={option === 2 ? theme.palette.primary[400] : undefined}>
+            스킨
+          </Stack>
+        </Stack>
+        {optionArray[option]}
       </Stack>
     </Stack>
   );
