d7fbd8bd916430549f1f80ba2d42abd73e441ef9
Chaeyeon1 <66813821+Chaeyeon1@users.noreply.github.com>
2023-09-15 09:48:55 +0900

    소셜 Login 테스트

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -22,6 +22,30 @@ import { useRecoilState, useRecoilValue, useSetRecoilState } from "recoil";
 import Snackbar from "../components/Snackbar.js";
 import { themeState, visitIdState } from "../states/common.js";
 import Fade from "react-reveal/Fade";
+import { defaultInstance } from "../apis/index.js";
+
+export const API_BASE_URL = defaultInstance;
+
+//서버에서 인증을 완료한 후에 프론트엔드로 돌아올 redirect uri (app.oauth2.authorized-redirect-uri와 일치해야 한다)
+export const OAUTH2_REDIRECT_URI =
+  "http://chaeyeonblog.s3-website-us-east-1.amazonaws.com/register";
+
+export const GOOGLE_AUTH_URL =
+  API_BASE_URL +
+  "/oauth2/authorization/google?redirect_uri=" +
+  OAUTH2_REDIRECT_URI;
+export const FACEBOOK_AUTH_URL =
+  API_BASE_URL +
+  "/oauth2/authorization/facebook?redirect_uri=" +
+  OAUTH2_REDIRECT_URI;
+export const NAVER_AUTH_URL =
+  API_BASE_URL +
+  "/oauth2/authorization/naver?redirect_uri=" +
+  OAUTH2_REDIRECT_URI;
+export const KAKAO_AUTH_URL =
+  API_BASE_URL +
+  "/oauth2/authorization/kakao?redirect_uri=" +
+  OAUTH2_REDIRECT_URI;
 
 const Login = () => {
   const theme = useRecoilValue(themeState);
@@ -90,7 +114,8 @@ const Login = () => {
           width="100%"
           flexDirection="row"
           paddingBottom="80px"
-          bgcolor="background.contractColor">
+          bgcolor="background.contractColor"
+        >
           {isNotSmall && (
             <Stack width="100%" paddingRight="200px">
               <Fade top>
@@ -98,7 +123,8 @@ const Login = () => {
                   direction="row"
                   justifyContent="center"
                   alignItems="center"
-                  style={{ width: "80%" }}>
+                  style={{ width: "80%" }}
+                >
                   <img
                     src={A}
                     alt="j"
@@ -117,7 +143,8 @@ const Login = () => {
                       paddingTop: "10em",
                       paddingLeft: "5em",
                       color: themes.palette.background.color,
-                    }}>
+                    }}
+                  >
                     나만의 블로그를 만들어보세요!
                   </Stack>
                 </Stack>
@@ -127,7 +154,8 @@ const Login = () => {
                   direction="row"
                   justifyContent="center"
                   alignItems="center"
-                  style={{ width: "80%" }}>
+                  style={{ width: "80%" }}
+                >
                   <Stack
                     style={{
                       width: "50%",
@@ -135,7 +163,8 @@ const Login = () => {
                       paddingTop: "10em",
                       paddingLeft: "5em",
                       color: themes.palette.background.color,
-                    }}>
+                    }}
+                  >
                     나만의 블로그를 만들어보세요!
                   </Stack>
                   <img
@@ -156,7 +185,8 @@ const Login = () => {
                   direction="row"
                   justifyContent="center"
                   alignItems="center"
-                  style={{ width: "80%" }}>
+                  style={{ width: "80%" }}
+                >
                   <img
                     src={C}
                     alt="j"
@@ -175,7 +205,8 @@ const Login = () => {
                       paddingTop: "10em",
                       paddingLeft: "5em",
                       color: themes.palette.background.color,
-                    }}>
+                    }}
+                  >
                     나만의 블로그를 만들어보세요!
                   </Stack>
                 </Stack>
@@ -185,7 +216,8 @@ const Login = () => {
                   direction="row"
                   justifyContent="center"
                   alignItems="center"
-                  style={{ width: "80%" }}>
+                  style={{ width: "80%" }}
+                >
                   <Stack
                     style={{
                       width: "50%",
@@ -193,7 +225,8 @@ const Login = () => {
                       paddingTop: "10em",
                       paddingLeft: "5em",
                       color: themes.palette.background.color,
-                    }}>
+                    }}
+                  >
                     나만의 블로그를 만들어보세요!
                   </Stack>
                   <img
@@ -221,7 +254,8 @@ const Login = () => {
               position: "fixed",
               right: 0,
               top: 0,
-            }}>
+            }}
+          >
             <Stack width="80%" gap="30px" style={{ alignItems: "center" }}>
               <Stack fontWeight="bold" fontSize="20px" color="primary.main">
                 Login
@@ -267,7 +301,8 @@ const Login = () => {
                 size="small"
                 sx={{
                   width: "80%",
-                }}>
+                }}
+              >
                 Login
               </Button>
               <Button
@@ -277,10 +312,17 @@ const Login = () => {
                 sx={{
                   width: "80%",
                   marginTop: "-20px",
-                }}>
+                }}
+              >
                 Sign Up
               </Button>
+              <Stack direction={"row"} spacing={3}>
+                <a href={GOOGLE_AUTH_URL}>구글</a>
+                <a href={KAKAO_AUTH_URL}>카카오</a>
+                <a href={NAVER_AUTH_URL}>네이버</a>
+              </Stack>
             </Stack>
+
             <Snackbar
               open={open}
               setOpen={setOpen}
