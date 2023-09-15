864912f74d5759f522f99650133c6880a92297fd
Chaeyeon1 <66813821+Chaeyeon1@users.noreply.github.com>
2023-09-15 17:05:51 +0900

    정규표현식 제거

--- frontend/src/user/oauth2/OAuth2RedirectHandler.js
+++ frontend/src/user/oauth2/OAuth2RedirectHandler.js
@@ -3,19 +3,19 @@ import { ACCESS_TOKEN } from "../../constants";
 import { Redirect } from "react-router-dom";
 
 class OAuth2RedirectHandler extends Component {
-  getUrlParameter(name) {
-    name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
-    var regex = new RegExp("[\\?&]" + name + "=([^&#]*)");
+  // getUrlParameter(name) {
+  //   name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+  //   var regex = new RegExp("[\\?&]" + name + "=([^&#]*)");
 
-    var results = regex.exec(this.props.location.search);
-    return results === null
-      ? ""
-      : decodeURIComponent(results[1].replace(/\+/g, " "));
-  }
+  //   var results = regex.exec(this.props.location.search);
+  //   return results === null
+  //     ? ""
+  //     : decodeURIComponent(results[1].replace(/\+/g, " "));
+  // }
 
   render() {
-    const token = this.getUrlParameter("token");
-    const error = this.getUrlParameter("error");
+    const token = "token";
+    const error = "error";
 
     if (token) {
       localStorage.setItem(ACCESS_TOKEN, token);
